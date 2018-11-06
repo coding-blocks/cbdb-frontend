07755e1b5d80fb4691e84a425927685b944085f1
Arnav Gupta <arnav@codingblocks.com>
2018-11-07 02:51:55 +0530

    add cb custom bootstrap
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
+      'importBootstrapCSS': false
+    }
   });
 
+  app.import('vendor/bootstrap.css')
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
