f617f13c4d25b46f9755550390f2e4621fd94524
Arnav Gupta <arnav@codingblocks.com>
2018-11-08 04:32:12 +0530

    add batch model
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ src/data/models/batch/model.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+const { Model } = DS;
+import { attr, belongsTo } from 'ember-decorators/data';
+
+export default class BatchModel extends Model {
+  @attr('date') startDate;
+  @attr('date') endDate;
+  @attr('string') lectureStartTime;
+  @attr('string') lectureEndTime;
+  @belongsTo('teacher') teacher;
+  @belongsTo('course') course;
+  @belongsTo('center') center;
+}
