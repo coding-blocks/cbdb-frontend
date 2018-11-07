07d9607ef4ab91d3ab3297094d8c85bcfe21a963
Arnav Gupta <arnav@codingblocks.com>
2018-11-07 15:25:55 +0530

    chuck our own blueprints, too much trouble
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- blueprints/component/files/__root__/__path__/component.ts
+++
@@ -1,5 +0,0 @@
-import Component from '@ember/component';
-
-export default class <%= classifiedModuleName %>Component extends Component {
-  // normal class body definition here
-};
--- blueprints/test-framework-detector.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(blueprint) {
-  blueprint.supportsAddon = function() {
-    return false;
-  };
-
-  return blueprint;
-};
