e93896b10adb7d7cf4e63c8568e5be8f0047cf73
Arnav Gupta <arnav@codingblocks.com>
2018-11-18 03:57:48 +0530

    remove jquery dependency:
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/data/models/application/adapter.js
+++ src/data/models/application/adapter.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
+import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
 const { JSONAPIAdapter } = DS;
 
-export default class ApplicationAdapter extends JSONAPIAdapter {
+export default class ApplicationAdapter extends JSONAPIAdapter.extend(AdapterFetch) {
   host = 'http://localhost:3232'
   namespace = 'api'
 }
